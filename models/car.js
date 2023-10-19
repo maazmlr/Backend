ee4043a6d804b71ae18664c35f95f65a7fde763e
Muhammad Maaz <75994722+maazmlr@users.noreply.github.com>
2023-10-19 14:24:50 +0500

    schema

--- models/car.js
+++ models/car.js
@@ -3,23 +3,6 @@ import mongoose,{mongo} from "mongoose";
 const {Schema} = mongoose;
 
 const carSchema = new mongoose.Schema({
-    carName:{
-        type:Schema.Types.String,
-        required:true,
-        unique:true
-    },
-    deatils: [
-      {
-        modelName: { type:Schema.Types.String,
-            required:true,
-            unique:true},
-        priceRange: {
-          min: Number,
-          max: Number
-        },
-        years:[Number]
-      }
-    ]
     
   });
 
